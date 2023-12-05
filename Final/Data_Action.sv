6cb009b32f90a156a6d0aac595b276d25062f517
xiuhu17 <mizhou17@yahoo.com>
2023-12-05 13:35:58 -0600

    update

--- Final/Data_Action.sv
+++ Final/Data_Action.sv
@@ -1,7 +1,8 @@
-module Data_Action(input logic[31:0] R0, input logic[47:0] Sub_Key);
+module Data_Action(input logic[63:0] Data, input logic[47:0] Sub_Key,
+                    output logic[63:0] Data_After_Action);
     // expand r0
     logic[47:0] R0_Expand;
-    Expand_Data r0_expand(.input_data(R0), .output_data(R0_Expand));
+    Expand_Data r0_expand(.input_data(Data[31:0]), .expand_data(R0_Expand));
 
     // first use r0 data do xor with key
     logic[47:0] Data_after_XOR;
@@ -12,9 +13,9 @@ module Data_Action(input logic[31:0] R0, input logic[47:0] Sub_Key);
     S_BOX_MAP_WRAPPER s_box_mix(.input_data(Data_after_XOR), .output_data(Data_after_S_BOX));
 
     // 
-    
-    
+    logic[31:0] Data_after_P_Box
+    P_BOX_Permutate p_box_per(.input_data(Data_after_S_BOX), .output_data(Data_after_P_Box));
 
-
-
+    //
+    assign Data_After_Action = {{Data[31:0]}, {Data[63:32] ^ Data_after_P_Box}};
 endmodule
--- Final/P_BOX_Permutate.sv
+++ Final/P_BOX_Permutate.sv
@@ -2,38 +2,38 @@ module P_BOX_Permutate(input logic[31:0] input_data,
                         output logic[31:0] output_data);
     always_comb
     begin
-        output_data [ 31 ] = input_data [ 16 ]
-        output_data [ 30 ] = input_data [ 25 ]
-        output_data [ 29 ] = input_data [ 12 ]
-        output_data [ 28 ] = input_data [ 11 ]
-        output_data [ 27 ] = input_data [ 3 ]
-        output_data [ 26 ] = input_data [ 20 ]
-        output_data [ 25 ] = input_data [ 4 ]
-        output_data [ 24 ] = input_data [ 15 ]
-        output_data [ 23 ] = input_data [ 31 ]
-        output_data [ 22 ] = input_data [ 17 ]
-        output_data [ 21 ] = input_data [ 9 ]
-        output_data [ 20 ] = input_data [ 6 ]
-        output_data [ 19 ] = input_data [ 27 ]
-        output_data [ 18 ] = input_data [ 14 ]
-        output_data [ 17 ] = input_data [ 1 ]
-        output_data [ 16 ] = input_data [ 22 ]
-        output_data [ 15 ] = input_data [ 30 ]
-        output_data [ 14 ] = input_data [ 24 ]
-        output_data [ 13 ] = input_data [ 8 ]
-        output_data [ 12 ] = input_data [ 18 ]
-        output_data [ 11 ] = input_data [ 0 ]
-        output_data [ 10 ] = input_data [ 5 ]
-        output_data [ 9 ] = input_data [ 29 ]
-        output_data [ 8 ] = input_data [ 23 ]
-        output_data [ 7 ] = input_data [ 13 ]
-        output_data [ 6 ] = input_data [ 19 ]
-        output_data [ 5 ] = input_data [ 2 ]
-        output_data [ 4 ] = input_data [ 26 ]
-        output_data [ 3 ] = input_data [ 10 ]
-        output_data [ 2 ] = input_data [ 21 ]
-        output_data [ 1 ] = input_data [ 28 ]
-        output_data [ 0 ] = input_data [ 7 ]
+        output_data [ 31 ] = input_data [ 16 ];
+        output_data [ 30 ] = input_data [ 25 ];
+        output_data [ 29 ] = input_data [ 12 ];
+        output_data [ 28 ] = input_data [ 11 ];
+        output_data [ 27 ] = input_data [ 3 ];
+        output_data [ 26 ] = input_data [ 20 ];
+        output_data [ 25 ] = input_data [ 4 ];
+        output_data [ 24 ] = input_data [ 15 ];
+        output_data [ 23 ] = input_data [ 31 ];
+        output_data [ 22 ] = input_data [ 17 ];
+        output_data [ 21 ] = input_data [ 9 ];
+        output_data [ 20 ] = input_data [ 6 ];
+        output_data [ 19 ] = input_data [ 27 ];
+        output_data [ 18 ] = input_data [ 14 ];
+        output_data [ 17 ] = input_data [ 1 ];
+        output_data [ 16 ] = input_data [ 22 ];
+        output_data [ 15 ] = input_data [ 30 ];
+        output_data [ 14 ] = input_data [ 24 ];
+        output_data [ 13 ] = input_data [ 8 ];
+        output_data [ 12 ] = input_data [ 18 ];
+        output_data [ 11 ] = input_data [ 0 ];
+        output_data [ 10 ] = input_data [ 5 ];
+        output_data [ 9 ] = input_data [ 29 ];
+        output_data [ 8 ] = input_data [ 23 ];
+        output_data [ 7 ] = input_data [ 13 ];
+        output_data [ 6 ] = input_data [ 19 ];
+        output_data [ 5 ] = input_data [ 2 ];
+        output_data [ 4 ] = input_data [ 26 ];
+        output_data [ 3 ] = input_data [ 10 ];
+        output_data [ 2 ] = input_data [ 21 ];
+        output_data [ 1 ] = input_data [ 28 ];
+        output_data [ 0 ] = input_data [ 7 ];
     end
 
 endmodule
